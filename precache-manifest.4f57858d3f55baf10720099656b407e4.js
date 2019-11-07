e8d08ced8c2acea00a7bda646f6a2b1e3e9bdc54
jubayer047 <akjubayer047@gmail.com>
2019-11-07 19:17:23 +0600

    Updates

---
+++ precache-manifest.4f57858d3f55baf10720099656b407e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc830b1b7973a42de7ce1d03d4fa68cc",
+    "url": "/reactProject01/index.html"
+  },
+  {
+    "revision": "dfa54f94c6daa8104b63",
+    "url": "/reactProject01/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dfa54f94c6daa8104b63",
+    "url": "/reactProject01/static/js/2.d4571d70.chunk.js"
+  },
+  {
+    "revision": "070c875bc43def89243d",
+    "url": "/reactProject01/static/js/main.2766cac7.chunk.js"
+  },
+  {
+    "revision": "838175c74e2755ad43cf",
+    "url": "/reactProject01/static/js/runtime-main.83fcfea5.js"
+  }
+]);
